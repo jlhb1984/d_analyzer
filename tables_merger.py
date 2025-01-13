97792924681ada315d754f0c5e60425f6b5bafe9
jlhb1984 <jlhb1984@gmail.com>
2025-01-13 10:38:53 -0500

    Se modificó módulo tables_merger exitosamente para elegir las 4 variables que habrá en la tabla fusionada.

--- tables_merger.py
+++ tables_merger.py
@@ -10,39 +10,26 @@ class Tables_merger:
     def merger(number_of_tables):       
                
         if number_of_tables==1:
-            table_name=input("\nDigita el nombre de la tabla 1: ")
-            df=pd.read_csv(table_name)
-            print("Información del dataframe: ")
-            print(df.info())         
-            df_unit_types=df[['Unit','Events','Date','Latitude','Longitude']]
-            print(df_unit_types)
-
-            option_null=input("Desea eliminar los datos nulos de la tabla: S/N \n")
-            if option_null=="S":
-                df_unit_types.dropna(axis=0,inplace=True)
-                print("df_table_info: ")
-                print(df_unit_types.info())
-                print(df_unit_types) 
-
-            Tables_merger.csv_reports(df_unit_types) 
-        
+             print("No tiene sentido fusionar 1 tabla.")
+           
         elif number_of_tables>1:
             table_name=input("\nDigita el nombre de la tabla 1: ")
-            df=pd.read_csv(table_name)
+            df=pd.read_csv("./reports/"+table_name)
             print("Información del dataframe: ")
             print(df.info())
-            df_unit_types=df[['Unit','Event','Time','GPS Fixed','GPS Age']]
+
+            #            df_unit_types=df[['Date','Speed','Send Time','Mileage']]
+            attributes=[]
+            print("\nSelecciona 4 atributos de la tabla:")
+
+            for i in range (0,4):
+                        attributes.append(input("\nDigita el nombre del atributo: "))
+            df_unit_types=df[[attributes[0],attributes[1],attributes[2],attributes[3]]]
                                     
-            for i in range(1,number_of_tables):
-                #UTS Sistemas Kalo MX.csv
-                #TSO Colombia - IBO IDCOM.csv
-                #TSO Colombia - IBO SEGTEC.csv
-                #TSO Mobile - Colombia.csv
-                #TSO Mobile - Peru.csv
-                #TSO Peru - FG SATELITAL.csv
+            for i in range(1,number_of_tables):                
                 table_name=input("\nDigita el nombre de la tabla: ")
-                df_aux=pd.read_csv(table_name)
-                df_aux_unit_types=df_aux[['Unit','Event','Time','GPS Fixed','GPS Age']]
+                df_aux=pd.read_csv("./reports/"+table_name)
+                df_aux_unit_types=df_aux[[attributes[0],attributes[1],attributes[2],attributes[3]]]
                 print("\nUnite types before concat:")
                 print("\n")
                 print(df_unit_types.info())                
@@ -60,20 +47,9 @@ class Tables_merger:
             
             Tables_merger.csv_reports(df_unit_types)           
 
-    def csv_reports(df):       
-        df_unit_types=df[['Unit','Event','Time','GPS Fixed','GPS Age']]
+    def csv_reports(df_unit_types):       
+        df_unit_types
         print(df_unit_types)
         exp_option=input("Desea generar un reporte en formato csv?\nS/N\n")
         if (exp_option=="S"):
-                df_unit_types.to_csv('Units_report.csv')        
-        look_option=input("Desea buscar una unidad:\nS/N\n")
-        while look_option!='N':
-                look_word=input("Digita la unidad a buscar en el campo Unit: ")
-                filter_df=df_unit_types[df_unit_types['Unit'].str.contains(look_word)]
-                print("Report of units "+look_word+":")
-                print(filter_df)
-                look_option=input("Desea buscar una unidad:\nS/N\n")
-        exp_option=input("Desea generar un reporte en formato csv?\nS/N\n")
-        if (exp_option=="S"):
-                filter_df.to_csv('Searched_unit_report.csv')  
-        
+                df_unit_types.to_csv('./reports/Units_report.csv')
